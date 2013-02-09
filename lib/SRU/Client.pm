72351511c7087bd83d405c936494fc20fabb840b
Emmanuel Di Pretoro <edipretoro@gmail.com>
2013-02-09 21:04:52 +0100

    * lib/SRU/Client.pm: adding some code to the 'explain' method

--- lib/SRU/Client.pm
+++ lib/SRU/Client.pm
@@ -3,13 +3,14 @@ use warnings;
 package SRU::Client;
 
 use Moo;
+use LWP::UserAgent;
 
 has 'base_url' => (
     is => 'ro'
 );
 
 sub explain {
-
+    return LWP::UserAgent->new->get( shift->base_url );
 }
 
 1;
