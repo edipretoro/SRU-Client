aa8fc453875cc7c09b54c87d25eac758f12a22d9
Emmanuel Di Pretoro <edipretoro@gmail.com>
2013-02-13 12:11:13 +0100

    * lib/SRU/Client.pm: adding the charset_map attribute and the code to use this attribute to convert bad response

--- lib/SRU/Client.pm
+++ lib/SRU/Client.pm
@@ -7,13 +7,31 @@ use LWP::UserAgent;
 use URI;
 use URI::QueryParam;
 use XML::Simple;
+use Unicode::Map;
 
 has 'base_url' => (
     is => 'ro'
 );
 
+has 'charset_map' => (
+    is => 'ro',
+    default => sub { undef }
+);
+
 sub explain {
-    return LWP::UserAgent->new->get( shift->base_url );
+    my ( $self ) = shift;
+    my $r = LWP::UserAgent->new->get( $self->base_url );
+    my $data;
+
+    if ($self->charset_map) {
+        my $map = Unicode::Map->new( $self->charset_map );
+        my $response = $map->to_unicode( $r->decoded_content );
+        $data = XMLin( $response );
+    } else {
+        $data = XMLin( $r->decoded_content );
+    }
+
+    return $data;
 }
 
 sub search {
