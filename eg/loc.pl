b47adbfe206d1ad431b01ca0cda6489731f8711f
Emmanuel Di Pretoro <edipretoro@gmail.com>
2013-02-20 14:01:29 +0100

    * eg/loc.pl: updating the script to search againt the catalog of the Library of Congress (LoC)

--- eg/loc.pl
+++ eg/loc.pl
@@ -6,12 +6,18 @@ use feature 'say';
 
 use Project::Libs;
 use SRU::Client;
+use MARC::Record;
+use MARC::File::XML ( BinaryEncoding => 'utf8', RecordFormat => 'USMARC' );
 
 my $sru_server = 'http://z3950.loc.gov:7090/voyager';
 my $searcher = SRU::Client->new( base_url => $sru_server );
 my $results = $searcher->search( 'dinosaur' );
-say 'We have ', $results->numberOfRecords, ' records.';
 
-while (my $result = $rseults->next() ) {
-    say $result->raw;
+say 'We found ', $results->numberOfRecords, ' books.';
+
+my @records = @{ $results->records };
+foreach my $record (@records) {
+    my $record = MARC::Record->new_from_xml( $record->recordData );
+    say $record->as_formatted;
+    say '-' x 55;
 }
