ccf6b48637dba87051451011f2cf8949b9986693
Emmanuel Di Pretoro <edipretoro@gmail.com>
2013-02-20 13:52:02 +0100

    * lib/SRU/Client/Response/searchRetrieve.pm: refactoring to used the brand new SRU::Client::Response::Record (using Moose instead of Moo to be able to express 'ArrayRef[SRU::Client::Response::Record]' and collecting the data from various XPath expressions

--- lib/SRU/Client/Response/searchRetrieve.pm
+++ lib/SRU/Client/Response/searchRetrieve.pm
@@ -2,8 +2,10 @@ package SRU::Client::Response::searchRetrieve;
 use strict;
 use warnings;
 
-use Moo;
+use Moose;
+use Moose::Util::TypeConstraints;
 use XML::LibXML;
+use SRU::Client::Response::Record;
 
 has 'version' => (
     is => 'ro'
@@ -13,6 +15,12 @@ has 'numberOfRecords' => (
     is => 'ro'
 );
 
+has 'records' => (
+    traits => [ 'Array' ],
+    is => 'ro',
+    isa => 'ArrayRef[SRU::Client::Response::Record]'
+);
+
 sub new_from_xml {
     my ( $class, $xml ) = @_;
 
@@ -20,9 +28,27 @@ sub new_from_xml {
     my $version = $dom->findvalue( '//zs:version' );
     my $numberOfRecords = $dom->findvalue( '//zs:numberOfRecords' );
 
+    my @records;
+    my $record_nodes = $dom->findnodes( '//zs:record' );
+    foreach my $record_node (@{ $record_nodes }) {
+        my $recordSchema = $record_node->findvalue( './zs:recordSchema' );
+        my $recordPacking = $record_node->findvalue( './zs:recordPacking' );
+        my $recordPosition = $record_node->findvalue( './zs:recordPosition' );
+        my $recordData = $record_node->find( './zs:recordData/node()' )->[0]->toString;
+
+        my $record = SRU::Client::Response::Record->new(
+            recordSchema => $recordSchema,
+            recordPacking => $recordPacking,
+            recordPosition => $recordPosition,
+            recordData => $recordData
+        );
+        push @records, $record;
+    }
+
     $class->new(
         version => $version,
         numberOfRecords => $numberOfRecords,
+        records => \@records
     );
 }
 
